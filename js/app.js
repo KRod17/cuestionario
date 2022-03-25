88aff878373cf8d63923e033167c2481fb2d753a
KRod17 <91706664+KRod17@users.noreply.github.com>
2022-03-24 23:50:48 -0500

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,194 @@
+/* 
+KEIVYS JOSE RODRIGUEZ GONZALEZ
+IG: _krod17
+*/
+const numeroPregunta = document.querySelector(".numero-pregunta");
+const textoPregunta = document.querySelector(".texto-pregunta");
+const opcContenedor = document.querySelector(".contenedor-opc");
+const answerIndicadorContenedor = document.querySelector(".indicador-respuesta");
+const inicioBox = document.querySelector(".inicio");
+const quizBox = document.querySelector(".quiz-box");
+const resultBox = document.querySelector(".resultados");
+
+let contadorPregunta = 0;
+let actualPregunta;
+let disponiblePregunta = [];
+let opcDisponible = [];
+let respuestaCorrecta=0;
+let intentos = 0;
+let level = 1; //PARA NIVELES
+
+// COLOCAR LAS PREGUNTAS DISPONIBLES EN EL ARRAY
+function setDisponiblePregunta(){
+	const totalPregunta = quiz.length;
+		for(let i=0;i<totalPregunta;i++){
+		disponiblePregunta.push(quiz[i]);
+		}
+}
+// CONTADOR DE PREGUNTAS
+function getNuevaPregunta(){
+	// NUMERO DE PREGUNTA
+	numeroPregunta.innerHTML = "Pregunta " + (contadorPregunta + 1) + " de " + quiz.length;
+
+	// OBTENER PREGUNTA ALEATORIA
+	const indexPregunta = disponiblePregunta[Math.floor(Math.random() * disponiblePregunta.length)]
+	actualPregunta = indexPregunta;
+	textoPregunta.innerHTML = actualPregunta.q;
+
+	// OBTENER LA POSICIÓN DEL INDEX DEL ARRAY
+	const index1 = disponiblePregunta.indexOf(indexPregunta);
+
+	// REMUEVE EL INDEX DE LA PREGUNTA DE LAS PREGUNTAS DISPONIBLES, LA PREGUNTA NO SE REPETIRÁ
+	disponiblePregunta.splice(index1,1);
+	const opcLen = actualPregunta.options.length
+
+	// COLOCA LAS OPCIONES EN EL ARRAY DE OPCIONES DISPONIBLE
+	for(let i=0;i<opcLen; i++){
+		opcDisponible.push(i);
+	}
+	
+	// CREAR LAS OPCIONES
+	opcContenedor.innerHTML = '';
+	let animacion = 0.2;
+
+	// CREAR LAS OPCIONES EN HTML
+	for(let i=0;i<opcLen;i++){
+
+		// OPCION ALEATORIA
+		const optionIndex = opcDisponible[Math.floor(Math.random() * opcDisponible.length)];
+
+		// OBTIENE LA POSICION DE 'optionIndex' DE 'opcDisponible'
+		const index2 = opcDisponible.indexOf(optionIndex);
+
+		// REMUEVE EL 'optionIndex' DE 'disponiblePregunta', LA OPCION NO SE REPETIRÁ
+		opcDisponible.splice(index2,1);
+		
+		const option = document.createElement("div");
+		option.innerHTML = actualPregunta.options[optionIndex];
+		option.id = optionIndex;
+		option.style.animationDelay = animacion + 's';
+		animacion = animacion + 0.5;
+		option.className = "opcion";
+		opcContenedor.appendChild(option)
+		option.setAttribute("onclick","getResult(this)");
+	}
+
+	contadorPregunta++;
+	
+}
+
+function getResult(element){
+	const id = parseInt(element.id);
+	if(id===actualPregunta.answer){
+		
+		// CAMBIAR A VERDE LA OPCION CORRECTA Y COLOCA UNA MARCA
+		element.classList.add("correct");
+		updateAnswerIndicador("correct");
+
+		respuestaCorrecta++;
+	}
+	else{
+
+		//CAMBIAR A ROJO LA OPCION ERRONEA Y COLOCA UNA MARCA
+		element.classList.add("wrong");
+		updateAnswerIndicador("wrong");
+	}
+	stopClick();
+}
+
+// NO SE PUEDEN ELEGIR MAS OPCIONES
+function stopClick(){
+	const opcLen = opcContenedor.children.length;
+	for(let i=0;i<opcLen;i++){
+		opcContenedor.children[i].classList.add("respondida");
+	}
+}
+
+function answerIndicador(){
+	answerIndicadorContenedor.innerHTML = '';
+	const totalPregunta = quiz.length;
+	for(let i=0;i<totalPregunta;i++){
+		const indicador = document.createElement("div");
+		answerIndicadorContenedor.appendChild(indicador);
+	}
+}
+function updateAnswerIndicador(markType){
+	answerIndicadorContenedor.children[contadorPregunta-1].classList.add(markType);
+}
+
+function next(){
+	if(contadorPregunta === quiz.length){
+		quizFin();
+	} else{
+		nivel();
+		getNuevaPregunta();
+	}
+}
+
+function quizFin(){
+
+	//OCULTA LA CAJA DEL QUIZ
+	quizBox.classList.add("hide");
+
+	// MUESTRA LA CAJA DE RESULTADO
+	resultBox.classList.remove("hide");
+	quizResultado();
+}
+
+// OBTENER LOS RESULTADOS
+function quizResultado(){
+	intentos++;
+	resultBox.querySelector(".total-preguntas").innerHTML = quiz.length;
+	resultBox.querySelector(".total-intentos").innerHTML = intentos;
+	const ganancias = respuestaCorrecta * 200000;
+	resultBox.querySelector(".total-ganancias").innerHTML = "$ " + ganancias;
+	resultBox.querySelector(".total-score").innerHTML = respuestaCorrecta + " / " + quiz.length;
+}
+
+function resetQuiz(){
+	
+	contadorPregunta = 0;
+	respuestaCorrecta=0;
+	level = 0;
+}
+
+function rendirse(){
+	quizBox.classList.add("hide");
+	resultBox.classList.remove("hide");
+	quizResultado();
+}
+
+function goHome(){
+
+	// OCULTA EL RESULTADO DE LA CAJA
+	resultBox.classList.add("hide");
+
+	// MUESTRA LA CAJA DE PREGUNTAS
+	inicioBox.classList.remove("hide");
+	resetQuiz();	
+}
+
+function startQuiz(){
+
+	// OCULTA LA CAJA DE INICIO
+	inicioBox.classList.add("hide");
+
+	// MUESTRA LA CAJA DEL QUIZ
+	quizBox.classList.remove("hide");
+
+	// CAMBIAREMOS TODAS LAS PREGUNTAS EN EL ARRAY
+	setDisponiblePregunta();
+
+	// LLAMAREMOS A LA NUEVA PREGUNTA
+	getNuevaPregunta();
+
+	// CREA INDICADORES DE RESPUESTA
+	answerIndicador();
+}
+
+function nivel(){
+console.log(level);
+		level++;
+
+		}
+
---
+++ js/preguntas.js
@@ -0,0 +1,142 @@
+/* 
+KEIVYS JOSE RODRIGUEZ GONZALEZ
+IG: _krod17
+*/
+const quiz = [
+
+	{ //Level 1  1/5
+		q:'¿Cuál es el lugar más frío de la tierra?',
+		options:['Antártida','Alemania','Rusia','Alaska'],
+		answer:0
+	},
+
+	{ //Level 1  2/5
+		q:'¿Quién escribió La Odisea?',
+		options:['Odin','Homero','Bart','Da Vinci'],
+		answer:1
+	},
+
+	{ //Level 1  3/5
+		q:'¿Cuál es el río más largo del mundo?',
+		options:['Nilo','Otún','Amazonas','Cauca'],
+		answer:2
+	},
+
+	{ //Level 1  4/5
+		q:'¿Cómo se llama la Reina del Reino Unido?',
+		options:['Isabel IV','Isabel III','Diana','Isabel II'],
+		answer:3
+	},
+
+	{ //Level 1  5/5
+		q:'¿En qué continente está Ecuador?',
+		options:['Asia','América','Europa','África'],
+		answer:1
+	},
+
+	{ //Level 2  1/5
+		q:'¿Qué tipo de animal es la ballena?',
+		options:['Mamífero','Anfibio','Cefalópodo','Peces'],
+		answer:0
+	},
+	{ //Level 2  2/5
+		q:'¿Cuándo acabó la II Guerra Mundial?',
+		options:['1900','1945','1977','1999'],
+		answer:1
+	},
+	{ //Level 2  3/5
+		q:'¿Quién es el autor de el Quijote?',
+		options:['Miguel de Cervantes','Edgar Allan Poe','Gabriel Garcia Marquez','Laura Restrepo'],
+		answer:0
+	},
+	{ //Level 2  4/5
+		q:'¿En qué país se encuentra la torre de Pisa?',
+		options:['Brasil','Canadá','Italia','Perú'],
+		answer:2
+	},
+	{ //Level 2  5/5
+		q:'¿Cómo se denomina el resultado de la multiplicación?',
+		options:['Resto','Binomio','Producto','Resultado'],
+		answer:2
+	},
+
+	{ //Level 3  1/5
+		q:'¿Cuál es el océano más grande?',
+		options:['Atlántico','Pacífico','Ártico','Antártico'],
+		answer:1
+	},
+	{ //Level 3  2/5
+		q:'¿Qué año llegó Cristóbal Colón a América?',
+		options:['1810','1900','1501','1492'],
+		answer:3
+	},
+	{ //Level 3  3/5
+		q:'¿Quién es el padre del psicoanálisis?',
+		options:['Sigmund Freud','Ismail Yildiz','Mónica Castro','Ramiro Vergara'],
+		answer:0
+	},
+	{ //Level 3  4/5
+		q:'¿Cuál es el río más largo del mundo?',
+		options:['Nilo','Otún','Amazonas','Cauca'],
+		answer:2
+	},
+	{ //Level 3  5/5
+		q:'¿Cuál es el disco más vendido de la historia?',
+		options:['Bohemian Rapsody','We will rock you','Thriller','Stand by me'],
+		answer:2
+	},
+
+	{ //Level 4  1/5
+		q:'¿Quién es el famoso Rey de Rock en los Estados Unidos?',
+		options:['Elvis Presley','Marilyn Manson','Michael Jackson','John Lennon'],
+		answer:0
+	},
+	{ //Level 4  2/5
+		q:'¿Qué deporte practicaba Michael Jordan?',
+		options:['Tenis','Baloncesto','Futbol','Baseball'],
+		answer:1
+	},
+	{ //Level 4  3/5
+		q:'¿Quién fue el presidente de España que gobernó desde el año 2011 hasta el 2018?',
+		options:['Francisco Franco','Felipe González','Luis Blanco','Mariano Rajoy'],
+		answer:3
+	},
+	{ //Level 4  4/5
+		q:'¿Cuántos años tiene un lustro?',
+		options:['10 años','5 años','2 años','8 años'],
+		answer:1
+	},
+	{ //Level 4  5/5
+		q:'¿Dónde se encuentra la capa de ozono?',
+		options:['Atmósfera','Hidosfera','Litosfera','Estratosfera'],
+		answer:0
+	},
+
+	{ //Level 5  1/5
+		q:'¿Cuándo empezó la Primera Guerra Mundial?',
+		options:['1520','1489','1914','1935'],
+		answer:2
+	},
+	{ //Level 5  2/5
+		q:'¿Cómo se llama el procedimiento de subir la bandera?',
+		options:['Alzar','Recoger','Jalar','Izar'],
+		answer:3
+	},
+	{ //Level 5  3/5
+		q:'¿Cuál es el primero de la lista de los números primos?',
+		options:['1','3','5','2'],
+		answer:3
+	},
+	{ //Level 5  4/5
+		q:'¿Qué día celebran los cristianos la festividad de la Epifanía de Jesús?',
+		options:['6 de enero','24 de diciembre','1 de enero','12 de junio'],
+		answer:2
+	},
+	{ //Level 5  5/5
+		q:'¿Cuál es la capital de Dinamarca?',
+		options:['Copenhague','Barcelona','Tirana','Luanda'],
+		answer:0
+	}
+]
+// KEIVYS JOSE RODRIGUEZ GONZALEZ
+// IG: _krod17
