f3d5a6336a5fca26f09aada1bc4474edf24a712c
KRod17 <91706664+KRod17@users.noreply.github.com>
2022-03-29 21:57:27 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -10,63 +10,52 @@ const inicioBox = document.querySelector(".inicio");
 const quizBox = document.querySelector(".quiz-box");
 const resultBox = document.querySelector(".resultados");
 
+const nivel1 = quiz;
+const nivel2 = quiz1;
+const nivel3 = quiz2;
+const nivel4 = quiz3;
+const nivel5 = quiz4;
+
 let contadorPregunta = 0;
 let actualPregunta;
 let disponiblePregunta = [];
 let opcDisponible = [];
 let respuestaCorrecta=0;
 let intentos = 0;
-let level = 0;
+let level = 1;
 let badAnswer = 0;
 let cuota = 200000;
 
 // COLOCAR LAS PREGUNTAS DISPONIBLES EN EL ARRAY
-
-function setDisponiblePregunta(){
-		
-	const totalPregunta = quiz.length;
-
-	let getLevel = level;
-		for(let i=0;i<totalPregunta;i++){
-		disponiblePregunta.push(quiz[i]);
-		
-	}
+function setDisponiblePregunta(){	
+	upNivel();
 }
 // CONTADOR DE PREGUNTAS
 function getNuevaPregunta(){
 	// NUMERO DE PREGUNTA
 	numeroPregunta.innerHTML = "Pregunta " + (contadorPregunta + 1) + " de " + quiz.length;
-
 	// OBTENER PREGUNTA ALEATORIA
 	const indexPregunta = disponiblePregunta[Math.floor(Math.random() * disponiblePregunta.length)]
 	actualPregunta = indexPregunta;
 	textoPregunta.innerHTML = actualPregunta.q;
-
 	// OBTENER LA POSICIÓN DEL INDEX DEL ARRAY
 	const index1 = disponiblePregunta.indexOf(indexPregunta);
-
 	// REMUEVE EL INDEX DE LA PREGUNTA DE LAS PREGUNTAS DISPONIBLES, LA PREGUNTA NO SE REPETIRÁ
 	disponiblePregunta.splice(index1,1);
 	const opcLen = actualPregunta.options.length
-
 	// COLOCA LAS OPCIONES EN EL ARRAY DE OPCIONES DISPONIBLE
 	for(let i=0;i<opcLen; i++){
 		opcDisponible.push(i);
 	}
-	
 	// CREAR LAS OPCIONES
 	opcContenedor.innerHTML = '';
 	let animacion = 0.2;
-
 	// CREAR LAS OPCIONES EN HTML
 	for(let i=0;i<opcLen;i++){
-
 		// OPCION ALEATORIA
 		const optionIndex = opcDisponible[Math.floor(Math.random() * opcDisponible.length)];
-
 		// OBTIENE LA POSICION DE 'optionIndex' DE 'opcDisponible'
 		const index2 = opcDisponible.indexOf(optionIndex);
-
 		// REMUEVE EL 'optionIndex' DE 'disponiblePregunta', LA OPCION NO SE REPETIRÁ
 		opcDisponible.splice(index2,1);
 		
@@ -79,27 +68,30 @@ function getNuevaPregunta(){
 		opcContenedor.appendChild(option)
 		option.setAttribute("onclick","getResult(this)");
 	}
-		contadorPregunta++;
+		contadorPregunta++;	
 }
 
 function getResult(element){
 	const id = parseInt(element.id);
 	if(id===actualPregunta.answer){
-		
 		// CAMBIAR A VERDE LA OPCION CORRECTA Y COLOCA UNA MARCA
 		element.classList.add("correct");
 		updateAnswerIndicador("correct");
 		respuestaCorrecta++;
+		level++;
+		upNivel();
 	}
-	else{
-		
+	else{	
 		//CAMBIAR A ROJO LA OPCION ERRONEA Y COLOCA UNA MARCA
 		element.classList.add("wrong");
 		updateAnswerIndicador("wrong");
 		badAnswer++;
+		level++;
+		upNivel();
 	}
 	badFinished();
-	stopClick();	
+	stopClick();
+	
 }
 
 // NO SE PUEDEN ELEGIR MAS OPCIONES
@@ -127,19 +119,15 @@ function next(){
 		quizFin();
 	} else{
 		getNuevaPregunta();
-		}
-	nivel();
+	}
 }
 
 function quizFin(){
-
 	//OCULTA LA CAJA DEL QUIZ
 	quizBox.classList.add("hide");
-
 	// MUESTRA LA CAJA DE RESULTADO
 	resultBox.classList.remove("hide");
 	quizResultado();
-	level= 0;
 }
 
 // OBTENER LOS RESULTADOS
@@ -150,13 +138,14 @@ function quizResultado(){
 	const ganancias = respuestaCorrecta * cuota;
 	resultBox.querySelector(".total-ganancias").innerHTML = "$ " + ganancias;
 	resultBox.querySelector(".total-score").innerHTML = respuestaCorrecta + " / " + quiz.length;
+	level=1;
+	contadorPregunta=0;
+	badAnswer=0;
 }
 
 function resetQuiz(){
-	
 	contadorPregunta = 0;
 	respuestaCorrecta=0;
-	//level = 0;
 }
 
 function rendirse(){
@@ -166,47 +155,68 @@ function rendirse(){
 }
 
 function goHome(){
-
 	// OCULTA EL RESULTADO DE LA CAJA
 	resultBox.classList.add("hide");
-
 	// MUESTRA LA CAJA DE PREGUNTAS
 	inicioBox.classList.remove("hide");
 	resetQuiz();	
 }
 
 function badFinished(){
-
 	if(badAnswer === 3){
 		cuota = 0;
 		rendirse();
 		badAnswer = 0;
-		
+		contadorPregunta=0;
 	}
 }
 
 function startQuiz(){
-
 	badFinished();
-
 	// OCULTA LA CAJA DE INICIO
 	inicioBox.classList.add("hide");
-
 	// MUESTRA LA CAJA DEL QUIZ
 	quizBox.classList.remove("hide");
-
 	// CAMBIAREMOS TODAS LAS PREGUNTAS EN EL ARRAY
 	setDisponiblePregunta();
-
 	// LLAMAREMOS A LA NUEVA PREGUNTA
 	getNuevaPregunta();
-	
 	// CREA INDICADORES DE RESPUESTA
 	answerIndicador();
-	nivel();
 }
 
-function nivel(){
-console.log(badAnswer);
-		level++;
+function upNivel(){
+	
+	switch(level){
+		case 1:
+			const totalPregunta = quiz.length;
+			for(let i=0;i<totalPregunta;i++){
+				disponiblePregunta.push(nivel1[i]);
+				}
+			break;
+		case 2:
+			const totalPregunta1 = quiz.length;
+			for(let i=0;i<totalPregunta1;i++){
+				disponiblePregunta.push(nivel2[i]);
+			}
+			break;
+		case 3:
+			const totalPregunta2 = quiz.length;
+			for(let i=0;i<totalPregunta2;i++){
+				disponiblePregunta.push(nivel3[i]);
+				}
+			break;
+		case 4:
+			const totalPregunta3 = quiz.length;
+			for(let i=0;i<totalPregunta3;i++){
+				disponiblePregunta.push(nivel4[i]);
+				}
+			break;
+		case 5:
+			const totalPregunta4 = quiz.length;
+			for(let i=0;i<totalPregunta4;i++){
+				disponiblePregunta.push(nivel5[i]);
+				}
+			break;
+	}
 }
